9c0c2d564ed6aa6cdc67decbcae04755452e3b7c
Kelvin Porter <kporter@protonmail.com>
2022-04-01 00:38:32 -0400

    newmongodb method name change, inmemory added

---
+++ internal/store/inmemory.go
@@ -0,0 +1,28 @@
+package store
+
+import "final"
+
+
+type InMemoryStore[MODEL Model] struct {
+	store map[string]Model
+}
+
+func NewInMemoryStore[MODEL Model]() *InMemoryStore[MODEL] {
+	return &InMemoryStore[MODEL]{
+		store: make(map[string]Model),
+	}
+}
+
+func (ims *InMemoryStore[MODEL]) Store(model MODEL) (err error) {
+	ims.store[model.Id()] = model
+	return nil
+}
+
+func (ims *InMemoryStore[MODEL]) FindById(id string) (result MODEL) {
+	return  ims.store[id].(MODEL)
+}
+
+func (ims *InMemoryStore[MODEL]) FindByKey(key string, value any) (result MODEL) {
+	final.LogFatal(nil, "Cannot use FindByKey for inmemory database we should fix this.")
+	return
+}
