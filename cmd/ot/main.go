1fde8171ae03d17f224ceceaf998c1a6c358fb1c
Kevin Chao <kevinchao2011@gmail.com>
2022-04-03 17:04:01 -0400

    inital cmd/ot boilerplate code

---
+++ cmd/ot/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+// forever
+// set up listening for incoming queue
+// take in all changes (ignore version)
+// compose deltas
+// declare queue to send back with name
+// send stuff back
+
+func beginListen() { //make queue and bind
+
+}
+
+func beginSpeak() {
+
+}
+
+func main() {
+	beginListen()
+	beginSpeak()
+	for {
+		// compose all deltas
+		// create complement of compose
+	}
+	
+}
