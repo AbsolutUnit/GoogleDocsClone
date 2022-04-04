ada7f41943e8c79fd0c06b9c627ebddb16047aab
Kevin Chao <kevinchao2011@gmail.com>
2022-04-03 21:01:56 -0400

    broken ot get code

--- internal/ot/ot.go
+++ internal/ot/ot.go
@@ -4,9 +4,17 @@ import (
 	"final"
 	"fmt"
 	"net/http"
+	"final/internal/rbmq"
 	"github.com/xxuejie/go-delta-ot/ot"
 	"github.com/fmpwizard/go-quilljs-delta/delta"
 )
+
+type OTServer struct {
+	config OTConfig
+	docs store.Repository[Document]
+	amqp rbmq.RabbitMq
+	fileServer ot.MultiFileNewServer
+}
 
 type Document struct {
 	contents delta.Delta
@@ -14,6 +22,14 @@ type Document struct {
 	clientIds []string
 } 
 
-func (d Document) Get (html string){
-
+func (ots OTServer) Get (html string){
+	//bold, italics, normal, line break
+	for _, op := range ots.Ops {
+		if op.attributes == nil {
+			if string(op.insert) == "\n" {
+				html += "<br/>"
+			}
+			html += fmt.Sprintf("<p>%s</p>", string(op.insert))
+		}
+	}
 }
