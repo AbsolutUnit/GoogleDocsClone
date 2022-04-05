33e82ddb1d34020afb2d0dc0c615908bba0558fc
Christopher Smith <christopher.s.smith@stonybrook.edu>
2022-04-05 04:36:04 -0400

    get rid of go stuff

--- README.md
+++
@@ -1,7 +0,0 @@
-# CSE 356 Final Project
-
-Things to note:
-
-- *Authentication* microservice: cmd/auth, internal/auth
-- Generic repository implementation: internal/store
-- TODO generic rabbitmq interface: internal/msg
--- internal/rbmq/broker.go
+++
@@ -1,9 +0,0 @@
-package rbmq
-
-import "github.com/streadway/amqp"
-
-type Broker interface {
-	Publish(exchangeName, exchangeType, key, message string) error
-	Consume(exchangeName, exchangeType, queueName, key string) <-chan amqp.Delivery
-	Close()
-}
