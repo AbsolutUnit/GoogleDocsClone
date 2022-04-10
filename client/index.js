cdda0eb41d2a5d037ab68b3776fa788b1d7c446d
Kevin Chao <kevinchao2011@gmail.com>
2022-04-10 15:41:15 -0400

    basic auth code still don't know the API calls

--- client/index.js
+++ client/index.js
@@ -5,8 +5,8 @@ const quill = new Quill('#editor', {
 quill.on('text-change', update); // might want editor-change
 
 // connect to event stream
-const ip = "backyardigans.cse356.compas.cs.stonybrook.edu"; 
-// const ip = "localhost:8080";
+//const ip = "backyardigans.cse356.compas.cs.stonybrook.edu"; 
+const ip = "localhost:8080";
 const id = generateId();
 const connUrl = "http://" + ip + "/connect/" + id;
 const eventSource = new EventSource(connUrl);
@@ -30,12 +30,52 @@ function update(delta) {
 const docbtn = document.getElementById("docbtn")
 const getUrl = "http://" + ip + "/doc/" + id;
 docbtn.onclick = (e) => {
-    console.log("clicked")
     fetch(getUrl).then(res => {
         res.text().then(function(text) {
             console.log("body", text)
         })
     })
+}
+//login and logout buttons are for testing
+const loginbtn = document.getElementById("loginbtn")
+loginbtn.onclick = (e) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/login");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    let data = {
+        username: "bob",
+        password: "the builder"
+    };
+    xhr.send(JSON.stringify(data));
+
+    //why tf doesn't this work 
+    // const params = {
+    //     username: "bob",
+    //     password: "the builder"
+    // }
+    // const options = {
+    //     method: 'POST',
+    //     body: JSON.stringify(params)
+    // }
+    // fetch('http://localhost:8080/login', options)
+    // .then(response => response.json())
+    // .then(response => {
+    //     // Do something with response.
+    // })
+}
+
+const logoutbtn = document.getElementById("logoutbtn")
+logoutbtn.onclick = (e) => {
+    console.log('logout hit!')
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8080/logout");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    let data = {
+        something: "doesn't matter",
+    };
+    xhr.send(JSON.stringify(data));
 }
 
 // receive transforms from server and apply them to editor
--- session/session.js
+++ session/session.js
@@ -4,44 +4,144 @@ const cors = require("cors");
 const WebSocket = require('ws')
 const ReconnectingWebSocket = require('reconnecting-websocket')
 const wsOptions = {WebSocket : WebSocket}
-// const QuillDeltaToHtmlConverter = require("quill-delta-to-html");
 const QuillDeltaToHtmlConverter = require('quill-delta-to-html').QuillDeltaToHtmlConverter;
 const Client = require('sharedb/lib/client')
 const richText = require('rich-text');
+const session = require('express-session')
+const MongoDBSession = require('connect-mongodb-session')(session)
+const mongoose = require('mongoose');
 
 const Connection = Client.Connection;
 Client.types.register(richText.type)
 
-// server setup
+const UserModel = require('./User')
+
+const mongoURI = 'mongodb+srv://kevinchao:fJkTywtN4BmDnL1x@cluster0.28ur3.mongodb.net/sessions?retryWrites=true&w=majority'
+mongoose.connect(mongoURI, {
+    useNewURLParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(res => {
+    console.log("MongoDB connected")
+})
+const store = new MongoDBSession({
+    uri: mongoURI,
+    collection: "mySessions"
+})
+
+// server setup & middleware
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static('client')); // serve static files
+//app.use(express.static(__dirname + '../client'));
+app.use(express.static('../client')); // serve static files
+app.use(express.urlencoded({extended: true}))
+app.use(
+    session({
+        secret: 'some key',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+)
+
+//middleware for maintaining login state
+//pass this middleware into any webpage that requires authentication
+const isAuth = (req, res, next) => {
+    if(req.session.isAuth) {
+        next()
+    } else {
+        console.log('not logged in!')
+        res.redirect('/')
+    }
+}
 
 // sharedb websocket connection setup
-//var socket = new WebSocket('ws://localhost:8081')
 const socket = new ReconnectingWebSocket('ws://localhost:8081', [], wsOptions)
 const connection = new Connection(socket)
 
-// create doc
-// const doc = connection.get("docs", "1")
-// doc.create([{insert: '\n'}], 'http://sharejs.org/types/rich-text/v1')
 
 // data structures
 const clients = {};
 
 // endpoints
 app.get('/connect/:id', handleConnect);
-app.post('/op/:id', handleOp);
-app.get('/doc/:id', handleDoc);
+app.post('/op/:id', isAuth, handleOp);
+app.get('/doc/:id', isAuth, handleDoc);
+app.post('/testcon', handleTestCon);
+app.get('/', handleStart)
+app.post('/addUser', handleAddUser)
+app.post('/login', handleLogin)
+app.post('/logout', handleLogout)
+
 
 app.listen(8080, () => { 
-    // console.log("Listening on port 8080") 
+    console.log("Listening on port 8080") 
 });
 
+//assumes body has user, password.
+async function handleAddUser(req, res, next) {
+    //TODO add error handling for missing password or username
+    const {username, password} = req.body
+    //assume users create unique usernames
+    let user = await UserModel.findOne({username})
+    if (user) {
+        console.log('username already taken!')
+        res.end()
+        return
+    }
+    user = new UserModel({
+        username,
+        password // TODO: do i want to encrypt this password
+    })
+    await user.save()
+    console.log('user saved')
+    res.end()
+}
+
+async function handleLogin(req, res, next) {
+    const {username, password} = req.body
+    const user = await UserModel.findOne({username});
+
+    if(!user) {
+        console.log("User does not exist!")
+    } else if (password != user.password) {
+        console.log("Wrong Password")
+    } else {
+        console.log("Successful login")
+        req.session.isAuth = true
+    }
+    res.end()
+    return
+
+}
+
+function handleLogout(req, res, next) {
+    console.log('logout called')
+    req.session.destroy((err) => {
+        if(err) throw err
+        res.redirect('/')
+    })
+}
+
+//testing function for purely adding cookie
+function handleTestCon(req, res, next) {
+    req.session.isAuth = true
+    res.set('X-CSE356','61f9d48d3e92a433bf4fc893');
+    //res.cookie('warm baked cookie', req.session.id) // cookie-parser is probably not necesary
+    res.send("hi welcome")
+    //console.log(req.session)
+    console.log("cookie id generated: ", req.session.id)
+}
+
+function handleStart(req, res, next) {
+    //req.session.isAuth = true;
+    //res.sendFile(path.join(__dirname, '../client/index.html'));
+}
+
 function handleConnect(req, res, next) {
-    console.log("handleConnect")
+    console.log("handleConnect called")
     // get client id
     const clientID = req.params.id
     // console.log("req: ", req)
@@ -67,7 +167,6 @@ function handleConnect(req, res, next) {
 
         doc.on('op', (op) => { // think about op batch
             //console.log("transformed op ", op)
-            // sse.send(JSON.stringify({data: op}))
             const data = `data: ${JSON.stringify([op])}\n\n`
             res.write(data)
         })
@@ -94,11 +193,6 @@ function handleOp(req, res, next) {
 }
 
 function handleDoc(req, res, next) {
-    // const headers = {
-    //     "X-CSE356": "61f9d48d3e92a433bf4fc893",
-    //     'Access-Control-Allow-Origin': '*',
-    // };
-    // res.writeHead(200, headers);
     res.set('X-CSE356','61f9d48d3e92a433bf4fc893');
     // console.log("handleDoc req.body: ", req)
     const clientID = req.params.id;
