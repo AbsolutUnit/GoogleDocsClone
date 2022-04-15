054c375d34e0e9ae87f001c5304c004bfa10840d
Christopher Smith <christopher.s.smith@stonybrook.edu>
2022-04-15 03:11:02 -0400

    clean imports

--- session/controllers/homeController.js
+++ session/controllers/homeController.js
@@ -1,17 +1,4 @@
-const WebSocket = require('ws');
-const ReconnectingWebSocket = require('reconnecting-websocket');
-const wsOptions = { WebSocket: WebSocket };
-const Client = require('sharedb/lib/client');
-const richText = require('rich-text');
-
-const Connection = Client.Connection;
-const collectionController = require('collectionController');
-Client.types.register(richText.type);
-
-const DocMapModel = require('../Models/Document');
-
-const socket = new ReconnectingWebSocket('ws://localhost:8081', [], wsOptions);
-const connection = new Connection(socket);
+const collectionController = require('collectionController')
 
 exports.renderPage = (req, res, next) => {
   let rankingOfKings = collectionController.getTopTen();
