4f145e20c68e371bb8b0cf4001966425e838dcb6
Christopher Smith <christopher.s.smith@stonybrook.edu>
2022-04-24 21:53:25 -0400

    k6 with auth test

---
+++ test/app-test.js
@@ -0,0 +1,77 @@
+import http from 'k6/http';
+import exec from 'k6/execution';
+import { check, sleep } from 'k6';
+// if we want node modules here we need a bundler...
+
+export const options = {
+  vus: 50,
+  duration:'30s',
+  // stages: [
+  //   { duration: '30s', target: 20 },
+  //   { duration: '1m30s', target: 10 },
+  //   { duration: '20s', target: 0 },
+  // ],
+};
+
+/* init code, run once per VU */
+
+const headers = {'Content-Type': 'application/json', 'Accept': '*/*'}
+
+// base urls
+const baseURL = 'http://209.94.56.214'
+const usersURL = baseURL + '/users'
+const collectionURL = baseURL + '/collection'
+const mediaURL = baseURL + '/media'
+const docURL = baseURL + '/doc'
+const homeURL = baseURL + '/home'
+// auth service endpoints
+const signupURL = usersURL + '/signup'
+const verifyURL = usersURL + '/verify'
+const loginURL = usersURL + '/login'
+const logoutURL = usersURL + '/logout'
+// document service endpoints
+const createURL = collectionURL + '/create'
+const deleteURL = collectionURL + '/delete'
+const listURL = collectionURL + '/list'
+const uploadURL = mediaURL + '/upload'
+const accessURL = mediaURL + '/access'
+const editURL = docURL + '/edit'
+const connectURL = docURL + '/connect'
+const opURL = docURL + '/op'
+const presenceURL = docURL + '/presence'
+const getURL = docURL + '/get'
+
+
+export function setup() {
+  // can use k6 api in here, not in init code
+  // whatever we return (pls return an object) becomes data arg in default func
+  // this is NOT run for each VU! (and so idk why we would use this...)
+}
+
+// this function will loop for duration seconds (see options const)
+export default function (data) {
+
+  let name = `VU${exec.vu.idInTest}`
+  let email = `VU${exec.vu.idInTest}@fake.com`
+  let password = 'KevinScaredOfVim'
+  
+  // signup/verify/login sequence
+  let res = http.post(signupURL, JSON.stringify({name: name, email: email, password: password}), {headers: headers})
+  check(res, { 'signup status was 200': (r) => r.status == 200 }); // response body can be empty per spec, just check status to pass test
+  sleep(1)
+  res = http.get(`${verifyURL}?name=${name}&key=${password}`) // password is backdoor key
+  check(res, { 'verify status was 200': (r) => r.status == 200 });
+  sleep(1)
+  res = http.post(loginURL, JSON.stringify({email: email, password: password}), {headers: headers})
+  check(res, {'user logged in (1st time)': (r) => JSON.parse(r.body).name == name && r.cookies })
+  sleep(1)
+  http.post(logoutURL)
+  check(res, { 'logout status was 200': (r) => r.status == 200 });
+  sleep(1)
+  res = http.post(loginURL, JSON.stringify({email: email, password: password}), {headers: headers})
+  check(res, {'user logged in (2nd time)': (r) => JSON.parse(r.body).name == name && r.cookies })
+
+
+  sleep(10);
+}
+// cookies will be cleared and TCP conns torn down before default func runs again
